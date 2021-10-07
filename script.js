7fe5fe2ef6134fdd77ba788f9f97ab1eb3c8381c
Jimmy Jam <jimmymano@mail.com>
2021-10-07 15:10:05 -0500

    added JS validation methods

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('testing');
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+function validateForm(){
+    // Validate form using constraint API
+    isValid = form.checkValidity();
+    console.log(isValid)
+    message.textContent = "Please fill out all fields";
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red'
+}
+
+function processFormData(e){
+    e.preventDefault();
+    // Validate form using constraint API
+    validateForm();
+    // Store data
+}
+
+// Event Listener
+form.addEventListener('submit',processFormData);
